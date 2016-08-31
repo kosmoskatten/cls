0e8b4610ccc1190110f33730f0fb1b8a1070f536
Patrik Sandahl <patrik.sandahl@ericsson.com>
2016-08-31 15:57:19 +0200

    Updated with NATS interfacing.

--- src/Cls.hs
+++ src/Cls.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   ) where
@@ -8,6 +9,10 @@ import Data.List ((\\), nub)
 import Data.Map (Map)
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import Options.Applicative
+import Network.Nats ( Nats, MsgQueue, withNats
+                    , defaultSettings, subscribe, nextMsg
+                    )
 
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.Map.Strict as Map
@@ -29,8 +34,37 @@ type Service    = Text
 type ServiceSet = ([Component], [Service])
 type Recipes    = Map Service Recipe
 
+data Options = Options
+  { natsUri    :: !String
+  , recipePath :: !FilePath
+  } deriving Show
+
+data Self = Self
+  { nats       :: !Nats
+  , queue      :: !MsgQueue
+  , recipes    :: !Recipes
+  , serviceSet :: !ServiceSet
+  }
+
 main :: IO ()
-main = putStrLn "Foo"
+main = do
+  opts    <- getOptions
+  recipes <- loadRecipes $ recipePath opts
+  case recipes of
+    Just rs -> withNats defaultSettings [natsUri opts] $ \n -> do
+      (_, q) <- subscribe n "system.cls.apply-start" Nothing
+      app $ Self { nats       = n
+                 , queue      = q
+                 , recipes    = rs
+                 , serviceSet = newServiceSet
+                 }
+    Nothing -> putStrLn "Cannot read/parse the recipes."
+
+app :: Self -> IO ()
+app self = do
+  msg <- nextMsg $ queue self
+  print msg
+  app self
 
 loadRecipes :: FilePath -> IO (Maybe Recipes)
 loadRecipes file = decode <$> LBS.readFile file
@@ -50,3 +84,30 @@ getRecipes ss rs = sequence $ map (lookup' rs) ss
 
 nubRecipes :: [Recipe] -> ServiceSet
 nubRecipes rs = (nub $ concatMap start rs, nub $ concatMap watch rs)
+
+getOptions :: IO Options
+getOptions = execParser options
+
+options :: ParserInfo Options
+options =
+  info (helper <*> optParser)
+    ( fullDesc
+      <> progDesc "Start the demo CLS."
+      <> header "CSIM Component Launcher Service"
+    )
+
+optParser :: Parser Options
+optParser =
+  Options <$> option auto
+    ( long "nats"
+      <> short 'n'
+      <> metavar "<NATS URI>"
+      <> help "NATS URI for connecting to the NATS."
+      <> value "nats://localhost:4222"
+    )
+          <*>  strOption
+    ( long "recipes"
+      <> short 'r'
+      <> metavar "<Recipe file>"
+      <> help "Recipes configuration file path."
+    )
